1ad54bb6830f7127e3575a05424b6473fe217d05
ethan-davies <git@ethann.dev>
2025-05-11 21:38:51 +0100

    chore: improve styles

--- components/NotificationComponent.tsx
+++ components/NotificationComponent.tsx
@@ -118,7 +118,7 @@ export default ErrorBoundary.wrap(function NotificationComponent({
                                 onClose!();
                             }}
                         >
-                            <svg width="24" height="24" viewBox="0 0 24 24" role="img" aria-labelledby="toastnotifications-notification-dismiss-title">
+                            <svg width="20" height="20" viewBox="0 0 24 24" role="img" aria-labelledby="toastnotifications-notification-dismiss-title">
                                 <title id="toastnotifications-notification-dismiss-title">Dismiss Notification</title>
                                 <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z" />
                             </svg>
